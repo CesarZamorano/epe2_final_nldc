b5f3016e427414c3fa24ddcc8a375ab717b9a812
Nicoleastorga94 <39605820+Nicoleastorga94@users.noreply.github.com>
2018-06-29 18:12:32 -0400

    añadiendo credenciales
    se añade credenciales a firebase

---
+++ src/app/firebase.info.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_INFO = {
+    apiKey: "AIzaSyDf7--O8ceBcCIgbx03epzBpmXES7dGhdQ",
+    authDomain: "epe2final.firebaseapp.com",
+    databaseURL: "https://epe2final.firebaseio.com",
+    projectId: "epe2final",
+    storageBucket: "",
+    messagingSenderId: "701453664710"
+};
