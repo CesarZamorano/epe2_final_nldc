c5dc30cb8e45aface5c663d30fbb6118ca564bd9
CesarZamorano <cesar.zamorano.buendia@gmail.com>
2018-06-29 17:47:57 -0400

    commit home & contact

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,138 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  messages: any;
+  private db: any;
+  model: any = {};
+  isEditing: boolean = false;
 
   constructor(public navCtrl: NavController) {
-
+    this.db = firebase.firestore();
+    this.loadData();
   }
+
+
+
+  loadData(){
+    this.getAllDocuments("mascota").then((e)=>{
+      this.messages = e;
+  });
+  }
+
+addMessage(){
+    if(!this.isEditing){
+    this.addDocument("mascota", this.model).then(()=>{
+      this.loadData();//refrescar la pantalla
+    });
+  }else{
+    this.updateDocument("mascota", this.model.$key, this.model).then(()=>{
+      this.loadData();//refrescar la pantalla
+    });
+  }
+  this.isEditing = false;
+  //limpiar formulario
+  this.model.title = '';
+  this.model.text = '';
+  this.model.diag = '';
+  this.model.fecha = '';
+
+}
+
+updateMessage(obj){
+  this.model = obj;
+  this.isEditing = true;
+}
+
+deleteMessage(key){
+  this.deleteDocument("mascota", key).then(()=>{
+    this.loadData();//refrescar pantalla
+    this.isEditing = false;
+  });
+}
+
+
+
+
+
+//Metodos de CRUD------------------------------------------------------------------------------------------
+
+getAllDocuments(collection: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+        this.db.collection(collection)
+            .get()
+            .then((querySnapshot) => {
+                let arr = [];
+                querySnapshot.forEach(function (doc) {
+                    var obj = JSON.parse(JSON.stringify(doc.data()));
+                    obj.$key = doc.id
+                    console.log(obj)
+                    arr.push(obj);
+                });
+
+                if (arr.length > 0) {
+                    console.log("Document data:", arr);
+                    resolve(arr);
+                } else {
+                    console.log("No such document!");
+                    resolve(null);
+                }
+
+
+            })
+            .catch((error: any) => {
+                reject(error);
+            });
+    });
+}
+
+deleteDocument(collectionName: string, docID: string): Promise<any> {
+  return new Promise((resolve, reject) => {
+      this.db
+          .collection(collectionName)
+          .doc(docID)
+          .delete()
+          .then((obj: any) => {
+              resolve(obj);
+          })
+          .catch((error: any) => {
+              reject(error);
+          });
+  });
+}
+
+addDocument(collectionName: string, dataObj: any): Promise<any> {
+  return new Promise((resolve, reject) => {
+      this.db.collection(collectionName).add(dataObj)
+          .then((obj: any) => {
+              resolve(obj);
+          })
+          .catch((error: any) => {
+              reject(error);
+          });
+  });
+}
+
+updateDocument(collectionName: string, docID: string, dataObj: any): Promise<any> {
+  return new Promise((resolve, reject) => {
+      this.db
+          .collection(collectionName)
+          .doc(docID)
+          .update(dataObj)
+          .then((obj: any) => {
+              resolve(obj);
+          })
+          .catch((error: any) => {
+              reject(error);
+          });
+  });
+}
+
 
 }
